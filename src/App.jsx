e2142ce7874ba5b381c8a0d6f28d02c36fc890b2
Harshit <itsharshit0005@gmail.com>
2025-02-07 20:23:42 +0530

    movie_picker

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+import movieCards from "./Components/movieCard"
+
+function App() {
+ return(
+  <>
+  <movieCards movie={{title: "xyz", release_date : "2023"}}/>
+  </>   
+ );
+     
+}
+
+
+
+export default App
